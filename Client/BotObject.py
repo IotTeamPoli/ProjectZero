f56cdb6e89f14017795974db9751eee4ae8114e3
LorenzoBellone <s258340@studenti.polito.it>
2020-05-18 17:40:59 +0200

    Update BotObject.py

--- Client/BotObject.py
+++ Client/BotObject.py
@@ -56,22 +56,280 @@ class IoTBot(object):
             This bot will also automatically send you alerts if dangerous values of gas are detected, if a suspicious movement
             is sensed when the alerts are activated or if the bluetooth beacon of an unwanted person is detected.""")
 
-        # List of handlers:
+        def check_presence(update, context):
+            # This command has no arguments. It returns all the bluetooth beacons detected in a specific house given the chat_id
+            # of the user.
+            house_list = requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            all_inside = requests.get(presence_server_and_port + "get_all_inside").json()
+            try:
+                for house in house_list:
+                    present = []
+                    for person in all_inside:
+                        if person["home"] == house:
+                            present.append("mac: " + person["mac"] + "\nname surname: " + person["name"] + " " + person[
+                                "surname"] + "\ndevice: " + person["device_name"])
+                    if len(present) != 0:
+                        text = "In house " + house + " the following people are present:\n" + "\n".join(present)
+                    else:
+                        text = "No one is inside the house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                    logging.info(
+                        "Presence of people requested for house " + house + " from user " + update.message.from_user.username)
+
+            except Exception as e:
+                logging.info("An error occurred in check people: " + str(e))
+
+        def check_white(update, context):
+            # This command has no arguments. It returns all the people that are situated in the white list for a specific house
+            # given the chat_id of the user.
+            house_list = requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            all_white = requests.get(presence_server_and_port + "print_all_whitelist").json()
+            try:
+                for house in house_list:
+                    present = []
+                    for person in all_white:
+                        if person["home"] == house:
+                            present.append(person["name"] + " " + person["surname"] + " " + person["mac"])
+                    if len(present) != 0:
+                        text = "For house " + house + " the following people are in the white list:\n" + "\n".join(
+                            present)
+                    else:
+                        text = "No one is in the white list for the house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                    logging.info(
+                        "White list requested for house " + house + " from user " + update.message.from_user.username)
+
+            except Exception as e:
+                logging.info("An error occurred in check white: " + str(e))
+
+        def check_black(update, context):
+            # This command has no arguments. It returns all the people that are situated in the black list for a specific house
+            # given the chat_id of the user.
+            house_list = requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            all_black = requests.get(presence_server_and_port + "print_all_blacklist").json()
+            try:
+                for house in house_list:
+                    present = []
+                    for person in all_black:
+                        if person["home"] == house:
+                            present.append(person["name"] + " " + person["surname"] + " " + person["mac"])
+                    if len(present) != 0:
+                        text = "For house " + house + " the following people are in the black list:\n" + "\n".join(
+                            present)
+                    else:
+                        text = "No one is in the black list for the house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                    logging.info(
+                        "Black list requested for house " + house + " from user " + update.message.from_user.username)
+
+            except Exception as e:
+                logging.info("An error occurred in check white: " + str(e))
+
+        def add_person_white(update, context):
+            # This command takes 3 arguments. It adds a person to the whitelist (people accepted and recognized in the house).
+            name = context.args[0]
+            surname = context.args[1]
+            mac = context.args[2]
+            if len(context.args) != 3:
+                text = "Please, insert a name, surname and mac address near the command /add_whitelist"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                logging.info("No arguments inserted near command /add_whitelist.")
+            unknowns = requests.get(presence_server_and_port + "print_all_unknown").json()  # list of unknowns
+            try:
+                for i in unknowns:
+                    if i["mac"] == mac:
+                        requests.put(presence_server_and_port + "rmv_this_person", data={"mac": mac})
+                        i["name"] = name
+                        i["surname"] = surname
+                        requests.put(presence_server_and_port + "add_to_white", data=i)
+                        context.bot.sendMessage(chat_id=update.effective_chat.id, text="person added to whitelist")
+                        logging.info(
+                            "A person was added to the white list from user: " + update.message.from_user.username)
+            except Exception as e:
+                logging.info("An error occurred in add person white: " + str(e))
+
+        def add_person_black(update, context):
+            # This command takes 3 arguments. It adds a person in the blacklist. Every time a blacklisted person is detected in
+            # the house an alert is sent to the user.
+            name = context.args[0]
+            surname = context.args[1]
+            mac = context.args[2]
+            if len(context.args) != 3:
+                text = "Please, insert a name, surname and mac address near the command /add_blackist"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                logging.info("No arguments inserted near command /add_blacklist.")
+            unknowns = requests.get(presence_server_and_port + "print_all_unknown").json()  # list of unknowns
+            try:
+                for i in unknowns:
+                    if i["mac"] == mac:
+                        requests.put(presence_server_and_port + "rmv_this_person", data={"mac": mac})
+                        i["name"] = name
+                        i["surname"] = surname
+                        requests.put(presence_server_and_port + "add_to_black", data=i)
+                        context.bot.sendMessage(chat_id=update.effective_chat.id, text="person added to blacklist")
+                        logging.info(
+                            "A person was added to the black list from user: " + update.message.from_user.username)
+            except Exception as e:
+                logging.info("An error occurred add person black: " + str(e))
+
+        def get_gas(update, context):
+            # Returns the gas sensed in all the houses that belong to the current user.
+            house_list = \
+            requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            # Request from the catalog for the API key to read the thingspeak channel
+            for house in house_list:
+                try:
+                    thing_params = requests.get(
+                        resource_server_and_port + "get_chr?id=" + house + "_Kitchen_gas").json()
+                    channel = thing_params["channel"]
+                    api_key = thing_params["key"]
+                    field = thing_params["field"]
+                    # Reading of the channel
+                    r = requests.get('https://api.thingspeak.com/channels/' + channel + '/fields/' + field +
+                                     '/last.json?api_key=' + api_key)
+                    # Reply to the user
+                    text = "Current gas value in house " + house + ": %.1f" % float(r.json()["field" + field])
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                except KeyError:
+                    logging.info("An error occurred: " + str(KeyError))
+                    text = "No gas devices detected for house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            if len(house_list) == 0:
+                text = "No houses detected with a working gas sensors"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            logging.info("Gas requested from user " + update.message.from_user.username)
+
+        def get_temperature(update, context):
+            # The argument is the room id. Returns the temperature sensed in that specific room of the house.
+            house_list = \
+            requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            if len(context.args) != 1:
+                text = "Please, insert the room id near the command /temperature"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                logging.info("No arguments inserted near command /temperature.")
+                return
+            room = context.args[0]
+            for house in house_list:
+                try:
+                    thing_params = requests.get(
+                        resource_server_and_port + "get_chr?id=" + house + "_" + room + "_temperature").json()
+                    channel = thing_params["channel"]
+                    api_key = thing_params["key"]
+                    field = thing_params["field"]
+                    r = requests.get('https://api.thingspeak.com/channels/' + channel + '/fields/' + field +
+                                     '/last.json?api_key=' + api_key)
+                    text = "Current temperature in room " + room + " for house " + house + ": %.1f Celsius degrees" \
+                           % float(r.json()["field" + field])
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                except KeyError:
+                    logging.info("An error occurred: " + str(KeyError))
+                    text = "No temperature devices detected for room " + room + " in house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            if len(house_list) == 0:
+                text = "No houses detected with a working temperature sensors"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            logging.info("Temperature requested from user " + update.message.from_user.username)
+
+        def get_humidity(update, context):
+            # The argument is the room id. Returns the humidity sensed in that specific room of the house.
+            if len(context.args) != 1:
+                text = "Please, insert the room id near the command /humidity"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                logging.info("No arguments inserted near command /humidity.")
+                return
+            house_list = \
+            requests.get(resource_server_and_port + "chat_house?id=" + str(update.effective_chat.id)).json()["house"]
+            room = context.args[0]
+            for house in house_list:
+                try:
+                    thing_params = requests.get(
+                        resource_server_and_port + "get_chr?id=" + house + "_" + room + "_humidity").json()
+                    channel = thing_params["channel"]
+                    api_key = thing_params["key"]
+                    field = thing_params["field"]
+                    r = requests.get('https://api.thingspeak.com/channels/' + channel + '/fields/' + field +
+                                     '/last.json?api_key=' + api_key)
+                    perc = "%"
+                    text = ("Current humidity in room " + room + " for house " + house + ": %.1f%s") % (
+                    float(r.json()["field" + field]),
+                    perc)
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                except KeyError:
+                    logging.info("An error occurred: " + str(KeyError))
+                    text = "No humidity devices detected for room " + room + " in house " + house
+                    context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            if len(house_list) == 0:
+                text = "No houses detected with a working humidity sensors"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+            logging.info("Humidity requested from user " + update.message.from_user.username)
+
+        def set_status(update, context):
+            # The arguments are the house id and the status "ON" or "OFF". If status is ON then the motion sensor has to notify
+            # a possible anomalous movement detected inside the house.
+            house = context.args[0]
+            status = context.args[1]
+            if len(context.args) != 2:
+                text = "Please, insert the house_id and the status near the command /status"
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+                logging.info("No arguments inserted near command /status.")
+            try:
+                requests.get(resource_server_and_port + "switch_status?id=" + house + "&status=" + status)
+                context.bot.sendMessage(chat_id=update.effective_chat.id, text="Status has been switched")
+                logging.info("Status of house " + house + "changed from user " + update.message.from_user.username)
+            except Exception as e:
+                logging.info("An error occurred: " + str(e))
+
+        def callback_black(context):
+            # This job queue periodically checks the blacklist to notify the user as soon as a blacklisted person is detected in
+            # the house.
+            blacks = requests.get(presence_server_and_port + "print_all_blacklist").json()
+            try:
+                for i in blacks:
+                    if i["present"] == "True":
+                        text = "WARNING\n unwanted person entered in " + i["home"] + " : " + i["name"] + " " + i[
+                            "surname"]
+                        chat = requests.get(resource_server_and_port + "house_chat?id=" + i["home"]).json()["chatID"]
+                        context.bot.sendMessage(chat_id=chat, text=text)
+                        logging.info("Blacklist person detected in house " + i["home"] + ". An alert was sent.")
+            except Exception as e:
+                logging.info("An error occurred in callback black: " + str(e))
+
         start_handler = CommandHandler('start', start)
         help_handler = CommandHandler('help', help)
+        gas_handler = CommandHandler('gas', get_gas)
+        temp_handler = CommandHandler("temperature", get_temperature)
+        hum_handler = CommandHandler("humidity", get_humidity)
+        # status_handler = CommandHandler("set_status", set_status)
+        checkpeople_handler = CommandHandler("checkpeople", check_presence)
+        whitelist_handler = CommandHandler("add_whitelist", add_person_white)
+        blacklist_handler = CommandHandler("add_blacklist", add_person_black)
+        checkwhite_handler = CommandHandler("checkwhitelist", check_white)
+        checkblack_handler = CommandHandler("checkblacklist", check_black)
 
         # List of dispatchers
         self.dispatcher.add_handler(start_handler)
         self.dispatcher.add_handler(help_handler)
+        self.dispatcher.add_handler(gas_handler)
+        self.dispatcher.add_handler(temp_handler)
+        self.dispatcher.add_handler(hum_handler)
+        # self.dispatcher.add_handler(status_handler)
+        self.dispatcher.add_handler(checkpeople_handler)
+        self.dispatcher.add_handler(whitelist_handler)
+        self.dispatcher.add_handler(blacklist_handler)
+        self.dispatcher.add_handler(checkwhite_handler)
+        self.dispatcher.add_handler(checkblack_handler)
+        self.job.run_repeating(callback_black, interval=60 * 5, first=60)
         self.updater.start_polling()
 
     def stop(self):
         self.updater.stop()
 
-    def sendAlert(self, msg):
-        self.bot.sendMessage(chat_id=692640171, text=msg)
+    def sendAlert(self, chatid, msg):
+        # In this method the alerts will be forwarded to the users
+        self.bot.sendMessage(chat_id=chatid, text=msg)
 
 if __name__ == '__main__':
+    # The main will be the mqtt subscriber for the alerts
     myBot = IoTBot()
     myBot.start()
     a = 0
@@ -82,3 +340,4 @@ if __name__ == '__main__':
         a += 1
         if a == 5:
             myBot.stop()
+            break
