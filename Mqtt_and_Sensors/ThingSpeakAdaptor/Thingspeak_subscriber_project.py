d26457f57e1dcb9075ea49984b4969904905a5fe
matteo.zh <s223012@studenti.polito.it>
2020-06-13 17:36:18 +0200

    Update Thingspeak_subscriber_project.py

--- Mqtt_and_Sensors/ThingSpeakAdaptor/Thingspeak_subscriber_project.py
+++ Mqtt_and_Sensors/ThingSpeakAdaptor/Thingspeak_subscriber_project.py
@@ -1,7 +1,7 @@
 from Thingspeak_Dosomething_project import *
 import time
 import json
-
+import requests
 
 # Global configuration variables
 config_file = '../../Catalog/configuration.json'
@@ -12,7 +12,9 @@ config = json.loads(configuration)
 service_address = config['servicecat_address']
 resource_id = config["catalog_list"][1]["resource_id"]
 res_address = requests.get(service_address + "get_address?id=" + resource_id).json()
+print(res_address)
 resource_address = "http://" + res_address["ip"] + ":" + str(res_address["port"]) + "/"
+
 
 if __name__ == "__main__":
 	test = DoSomething("IoTeamThingSpeakAdaptor", service_address)
