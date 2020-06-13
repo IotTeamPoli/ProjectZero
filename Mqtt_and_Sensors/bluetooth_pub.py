00f4bf820265f45f58e979d5e14820f358333f84
Ninja <s223012@studenti.polito.it>
2020-06-13 11:31:34 +0200

    Update control presence

--- Mqtt_and_Sensors/bluetooth_pub.py
+++ Mqtt_and_Sensors/bluetooth_pub.py
@@ -2,18 +2,44 @@ import bluetooth
 import requests
 import time
 import json
+import paho.mqtt.client as PahoMQTT
 
-FILENAME = "config_sensors.json"
-with open(FILENAME, "r") as f:
-    d = json.load(f)
-    IP_RASP = d["servicecat_ip"]
-    house_id = d["house_id"]
 
-PRESENCE = "../Catalog/configuration.json"
-with open(PRESENCE, "r") as f:
-    d = json.load(f)
-    CATALOG_NAME = d["catalog_list"][2]["presence_id"]
+class MyPublisher:
+    """
+    default broker and port are provided
+    create publisher:
+    pub = MyPublisher("ID of pub", broker, port)
+    pub.start()
+    pub.myPublish("topic",values)
+    pub.stop()
+    """
 
+    def __init__(self, clientID, broker="192.168.1.254", port=1884):
+        self.clientID = clientID
+        self.port = port
+        # create an instance of paho.mqtt.client
+        self._paho_mqtt = PahoMQTT.Client(self.clientID, False)
+        # register the callback
+        self._paho_mqtt.on_connect = self.myOnConnect
+        self.messageBroker = broker
+        # self.messageBroker = 'iot.eclipse.org'
+
+    def start(self):
+        # manage connection to broker
+        self._paho_mqtt.connect(self.messageBroker, self.port)
+        self._paho_mqtt.loop_start()
+
+    def stop(self):
+        self._paho_mqtt.loop_stop()
+        self._paho_mqtt.disconnect()
+
+    def myPublish(self, topic, message):
+        # publish a message with a certain topic qos 2
+        self._paho_mqtt.publish(topic, message, 2)
+
+    def myOnConnect(self, paho_mqtt, userdata, flags, rc):
+        print("Connected to %s with result code: %d" % (self.messageBroker, rc))
 
 def list_search(get_uri, add_uri, rmv, mac_lists):
     present = []
@@ -54,19 +80,35 @@ def register_unknown(address, device, add_to_unknown):
 
 
 def main():
-    from_config = connection(IP_RASP, CATALOG_NAME)
+    FILENAME = "config_sensors.json"
+    with open(FILENAME, "r") as f:
+        d = json.load(f)
+        IP_RASP = d["servicecat_ip"]
+        house_id = d["house_id"]
 
-    # default methods
-    uri_get_whitelist = from_config + "/print_all_whitelist"
-    uri_get_blacklist = from_config + "/print_all_blacklist"
-    uri_get_unknownlist = from_config + "/print_all_unknown"
-    uri_add_unknown = from_config + "/add_to_unknown"
-    uri_add_white = from_config + "/add_to_white"
-    uri_add_black = from_config + "/add_to_black"
-    rmv = from_config + "/rmv_this_person"
+    RESOURCE = "../Catalog/configuration.json"
+    with open(RESOURCE, "r") as f:
+        d = json.load(f)
+        CATALOG_NAME = d["catalog_list"][2]["resource_id"]
 
-    mac_list = []  # detected macs
-    presence_macs = []  # known macs
+    from_config = IP_RASP
+    broker = requests.get(from_config + "get_broker").json()
+
+    port_broker = requests.get(from_config + "get_broker_port").json()
+    port = port_broker
+
+    resource_ip = requests.get(from_config + "get_address?id=" + CATALOG_NAME).json()
+    print(from_config + "get_address?id=" + CATALOG_NAME)
+
+    # Resource
+    resource_cat = resource_ip["ip"] + ":" + str(resource_ip["port"])
+
+    topic_presence = requests.get("http://" + resource_cat + "/get_topic?id=house1_Kitchen_temperature").json()
+    print("http://" + resource_cat + "/get_topic?id=house1_Kitchen_temperature")
+
+    presence_pub = myPublisher("presence",broker,port)
+
+    mac_list = []
 
     while True:
         # scanning all present devices and create a list of present macs
@@ -80,19 +122,9 @@ def main():
                 print("\t%s - %s" % (mac, device_name))
             except Exception as e:
                 print('error 1: ', e)
-        try:
-            present_a = list_search(uri_get_whitelist, uri_add_white, rmv, mac_list)
-            present_b = list_search(uri_get_blacklist, uri_add_black, rmv, mac_list)
-            present_c = list_search(uri_get_unknownlist, uri_add_unknown, rmv, mac_list)
-            presence_macs = present_a + present_b + present_c
-        except Exception as e:
-            print('error 2: ', e)
 
-        for mac, device_name in nearby_devices:
-            if mac not in presence_macs:  # if mac is unknown
-                register_unknown(mac, device_name, uri_add_unknown)
+        presence_pub.myPublish(topic_presence, json.dumps())
 
-        mac_list.clear()
         time.sleep(50)
 
 
