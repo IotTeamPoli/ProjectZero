754ee443c04b58300482a11898c14c94000b838b
LorenzoBellone <s258340@studenti.polito.it>
2020-07-01 11:46:19 +0200

    Update Chat_ID_Bot.py

--- Client/Chat_ID_Bot.py
+++ Client/Chat_ID_Bot.py
@@ -5,10 +5,11 @@ TOKEN = "801308577:AAFpc5w-nzYD1oHiY-cj_fJVaKH92P4uLCI"
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
-bot = telegram.bot(token=TOKEN)
+bot = telegram.Bot(token=TOKEN)
 
 def chat(context, update):
 	print(update.effective_chad.id)
+
 
 chat_handler = CommandHandler("chat", chat)
 dispatcher.add_handler(chat_handler)
